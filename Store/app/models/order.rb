7e45378619a0c3fa7600f2dc573c192f9ea8a616
CarlosMedrano27 <cemg_neto@hotmail.com>
2019-03-20 09:13:09 -0600

    seed of order detail moved to model

--- Store/app/models/order.rb
+++ Store/app/models/order.rb
@@ -2,4 +2,15 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details
   has_many :products, through: :order_details
+
+  def seed_detail(product, last_order)
+    product.each do |val|
+      @detail = OrderDetail.new(order: last_order,
+                                product: val.product,
+                                quantity: val.quantity,
+                                price: val.price,
+                                status: 'P')
+      CarShop.where(user: usr, product: val.product).destroy_all if @detail.save
+    end
+  end
 end
