3e530bd65f2c63257a783f31df7abc92a2c6677b
CarlosMedrano27 <cemg_neto@hotmail.com>
2019-03-21 12:24:18 -0600

    working in production

--- Store/config/environments/production.rb
+++ Store/config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # SMTP settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['MAIL_USER'],
+    password: ENV['MAIL_PASSWORD'],
+    address: 'smtp.sendgrid.net',
+    domain: 'localhost:3000',
+    port: '587',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
