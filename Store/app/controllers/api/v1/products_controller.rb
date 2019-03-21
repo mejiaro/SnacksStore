8f28c65ce03ee6a3a79027ae2cd2152e2a79b4bb
CarlosMedrano27 <cemg_neto@hotmail.com>
2019-03-21 13:47:01 -0600

    fixed fixtures

--- Store/app/controllers/api/v1/products_controller.rb
+++ Store/app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,14 @@ module API
   module V1
     class ProductsController < ApplicationController
       def index
-        @product = Product.where("status='A'").search(params[:term], params[:page], params[:sort], params[:category])
+        @product = Product.where("status='A'")
+        @product = @product.category_scope(@category) if @category
+        @product = @product.sort_scope('product_name ASC') unless @sort
+        @product = @product.sort_scope(@sort) if @sort
+        @product = @product.term_scope(@term) if @term
+        @product = @product.page_scope(@page).includes(
+          :category, :like_products
+        )
         respond_to do |format|
           format.json { render json: @product }
         end
